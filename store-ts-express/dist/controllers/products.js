cd36a7c1a78b4f77d5036c9b4a9394306689c21d
Percival Lucena <percival.silva.de.lucena1@ibm.com>
2025-02-09 10:50:35 -0300

    v5

--- store-ts-express/dist/controllers/products.js
+++ store-ts-express/dist/controllers/products.js
@@ -1,35 +1,74 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.removeProducts = exports.updateProducts = exports.getProducts = exports.createProducts = void 0;
 const product_1 = require("../models/product");
+const pg_1 = require("pg");
+const pool = new pg_1.Pool({
+    user: 'root',
+    host: 'localhost',
+    database: 'ecommerce',
+    password: 'root',
+    port: 5432,
+});
 const Products = [];
-const createProducts = (req, resp, next) => {
-    const createdProduct = new product_1.Product(req.body.id, req.body.title, req.body.price);
-    Products.push(createdProduct);
-    resp.status(201).json({ product: createdProduct });
-};
+const createProducts = (req, resp, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id, title, price } = req.body;
+    const createdProduct = new product_1.Product(id, title, price);
+    try {
+        yield pool.query('INSERT INTO products (id, title, price) VALUES ($1, $2, $3)', [id, title, price]);
+        resp.status(201).json({ product: createdProduct });
+    }
+    catch (error) {
+        next(error);
+    }
+});
 exports.createProducts = createProducts;
-const getProducts = (req, resp, next) => {
-    resp.json({ products: Products });
-};
+const getProducts = (req, resp, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const result = yield pool.query('SELECT * FROM products');
+        const products = result.rows;
+        resp.json({ products });
+    }
+    catch (error) {
+        next(error);
+    }
+});
 exports.getProducts = getProducts;
-const updateProducts = (req, resp, next) => {
+const updateProducts = (req, resp, next) => __awaiter(void 0, void 0, void 0, function* () {
     const productId = req.params.id;
-    const productIndex = Products.findIndex(prod => prod.id === parseInt(productId));
-    if (productIndex < 0) {
-        throw new Error('Product not found');
+    const { id, title, price } = req.body;
+    try {
+        const result = yield pool.query('UPDATE products SET id = $1, title = $2, price = $3 WHERE id = $4 ', [id, title, price, productId]);
+        if (result.rowCount === 0) {
+            throw new Error('Product not found');
+        }
+        resp.json({ product: result.rows[0] });
     }
-    Products[productIndex] = new product_1.Product(req.body.id, req.body.title, req.body.price);
-    resp.json({ products: Products });
-};
+    catch (error) {
+        next(error);
+    }
+});
 exports.updateProducts = updateProducts;
-const removeProducts = (req, resp, next) => {
+const removeProducts = (req, resp, next) => __awaiter(void 0, void 0, void 0, function* () {
     const productId = req.params.id;
-    const productIndex = Products.findIndex(prod => prod.id === parseInt(productId));
-    if (productIndex < 0) {
-        throw new Error('Product not found');
+    try {
+        const result = yield pool.query('DELETE FROM products WHERE id = $1 ', [productId]);
+        if (result.rowCount === 0) {
+            throw new Error('Product not found');
+        }
+        resp.json({ message: 'Product removed', product: result.rows[0] });
     }
-    Products.splice(productIndex, 1);
-    resp.json({ products: Products });
-};
+    catch (error) {
+        next(error);
+    }
+});
 exports.removeProducts = removeProducts;
--- store-ts-express/src/controllers/products.ts
+++ store-ts-express/src/controllers/products.ts
@@ -1,34 +1,67 @@
 import { Request, RequestHandler, Response, NextFunction } from 'express';
 import{Product} from '../models/product';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    user: 'root',
+    host: 'localhost',
+    database: 'ecommerce',
+    password: 'root',
+    port: 5432,
+});
 
 const Products: Product[] = [];
 
-export const createProducts: RequestHandler = (req: Request, resp: Response, next: NextFunction )=> {
-    const createdProduct = new Product(req.body.id, req.body.title, req.body.price);
-    Products.push(createdProduct);
-    resp.status(201).json({product: createdProduct});
+export const createProducts: RequestHandler = async (req: Request, resp: Response, next: NextFunction )=> {
+    const { id, title, price } = req.body;
+    const createdProduct = new Product(id, title, price);
+
+    try {
+        await pool.query('INSERT INTO products (id, title, price) VALUES ($1, $2, $3)', [id, title, price]);
+        resp.status(201).json({ product: createdProduct });
+    } catch (error) {
+        next(error);
+    }
 }   
 
-export const getProducts: RequestHandler = (req: Request, resp: Response, next: NextFunction )=> {
-    resp.json({products: Products});
+export const getProducts: RequestHandler = async (req: Request, resp: Response, next: NextFunction )=> {
+    try {
+        const result = await pool.query('SELECT * FROM products');
+        const products = result.rows;
+        resp.json({ products });
+    } catch (error) {
+        next(error);
+    }
 }
 
-export const updateProducts: RequestHandler = (req: Request, resp: Response, next: NextFunction )=> {
+export const updateProducts: RequestHandler = async (req: Request, resp: Response, next: NextFunction )=> {
     const productId = req.params.id;
-    const productIndex = Products.findIndex(prod => prod.id === parseInt(productId));
-    if(productIndex < 0) {
-        throw new Error('Product not found');
+    const { id, title, price } = req.body;
+
+    try {
+        const result = await pool.query(
+            'UPDATE products SET id = $1, title = $2, price = $3 WHERE id = $4 ',
+            [id, title, price, productId]
+        );
+        if (result.rowCount === 0) {
+            throw new Error('Product not found');
+        }
+        resp.json({ product: result.rows[0] });
+    } catch (error) {
+        next(error);
     }
-    Products[productIndex] = new Product(req.body.id, req.body.title, req.body.price);
-    resp.json({products: Products});
 }
 
-export const removeProducts: RequestHandler = (req: Request, resp: Response, next: NextFunction )=> {    
+export const removeProducts: RequestHandler = async (req: Request, resp: Response, next: NextFunction )=> {    
     const productId = req.params.id;
-    const productIndex = Products.findIndex(prod => prod.id === parseInt(productId));
-    if(productIndex < 0) {
-        throw new Error('Product not found');
+
+    try {
+        const result = await pool.query('DELETE FROM products WHERE id = $1 ', [productId]);
+        if (result.rowCount === 0) {
+            throw new Error('Product not found');
+        }
+        resp.json({ message: 'Product removed', product: result.rows[0] });
+    } catch (error) {
+        next(error);
     }
-    Products.splice(productIndex, 1);
-    resp.json({products: Products});
 }
