b42002b673ab1b53e4aeef3f999371760f757fd7
Percival Lucena <percival.silva.de.lucena1@ibm.com>
2025-02-08 21:59:18 -0300

    v3

--- store-ts/src/controllers/products.ts
+++ store-ts/src/controllers/products.ts
@@ -9,3 +9,26 @@ export const createProducts: RequestHandler = (req: Request, resp: Response, nex
     resp.status(201).json({product: createdProduct});
 }   
 
+export const getProducts: RequestHandler = (req: Request, resp: Response, next: NextFunction )=> {
+    resp.json({products: Products});
+}
+
+export const updateProducts: RequestHandler = (req: Request, resp: Response, next: NextFunction )=> {
+    const productId = req.params.id;
+    const productIndex = Products.findIndex(prod => prod.id === parseInt(productId));
+    if(productIndex < 0) {
+        throw new Error('Product not found');
+    }
+    Products[productIndex] = new Product(req.body.id, req.body.title, req.body.price);
+    resp.json({products: Products});
+}
+
+export const removeProducts: RequestHandler = (req: Request, resp: Response, next: NextFunction )=> {    
+    const productId = req.params.id;
+    const productIndex = Products.findIndex(prod => prod.id === parseInt(productId));
+    if(productIndex < 0) {
+        throw new Error('Product not found');
+    }
+    Products.splice(productIndex, 1);
+    resp.json({products: Products});
+}
--- store-ts/src/routes/products.ts
+++ store-ts/src/routes/products.ts
@@ -1,15 +1,17 @@
 import {Router} from 'express';
 
-import{ createProducts} from '../controllers/products';
+import{ createProducts, getProducts, updateProducts, removeProducts} from '../controllers/products';
+
+
 
 const router = Router();
 
 router.post('/', createProducts);
 
-router.get('/');
+router.get('/', getProducts);
 
-router.patch('/:id');
+router.patch('/:id', updateProducts);
 
-router.delete('/:id');
+router.delete('/:id', removeProducts);
 
 export default router;  
